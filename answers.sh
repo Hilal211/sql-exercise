23265b2105a30e54c1730ae9790d53a61a70c35c
hilal masri <hilal1922001@gmail.com>
2021-03-18 22:23:00 +0200

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,15 @@
+select Name from students;
+select * from students
+where Age>30;
+select Name from students
+where Gender like'F'
+And age like'30';
+insert into students VALUES(8,"Hilal","20",'M',500);
+update students
+set Points=400
+where Name="Basma";
+update students
+set Points=150
+where Name="Alex";
+
+
